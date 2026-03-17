fdec15c1bf3b8be442f10e6359d26df9879fc87d
VeryNyu <gustavohuerta96@gmail.com>
2026-03-17 14:11:11 -0600

    Run and Jump

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -8,17 +8,24 @@ const JUMP_VELOCITY = -400.0
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
+		$AnimatedSprite2D.play("double_jump")
 		velocity += get_gravity() * delta
+	else:
+		$AnimatedSprite2D.play("idle")
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("move_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
+		if direction > 0:
+			$AnimatedSprite2D.flip_h = false
+		elif direction < 0:
+			$AnimatedSprite2D.flip_h = true
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
