0173fcaf207db3865e93394ef8c1deb89d051974
VeryNyu <gustavohuerta96@gmail.com>
2026-03-29 17:15:22 -0600

    Timer++

---
+++ Scripts/UI/hud.gd
@@ -0,0 +1,28 @@
+extends Control
+
+var time_elapsed: float = 0.0
+var is_stopped: bool = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	hide()
+	time_elapsed = 0.0
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if !is_stopped:
+		time_elapsed += delta
+		$Time/Time.text = str(time_elapsed).pad_decimals(2)
+
+
+func Game_Start():
+	time_elapsed = 0.0
+	$Score/Score.text = "00000"
+	show()
+	is_stopped = false
+
+
+func stop() -> void:
+	is_stopped = true
--- Scripts/UI/ui.gd
+++ Scripts/UI/ui.gd
@@ -6,4 +6,5 @@ signal Game_Start
 func _on_game_start(key: String) -> void:
 	print("UI Signal Reached, Forwarding " + key)
 	Game_Start.emit(key)
+	$CanvasLayer/Hud.Game_Start()
 	$CanvasLayer/MenuManager.pause(false)
