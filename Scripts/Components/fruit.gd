97d7dfe4e4f87163f396f1b856da6e71409a2e02
VeryNyu <gustavohuerta96@gmail.com>
2026-04-14 10:13:03 -0600

    fruit++

---
+++ Scripts/Components/fruit.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+signal collect
+
+var body: String = "apple"
+var value: int
+
+
+func _on_body_entered(_body: Node2D) -> void:
+	$AnimatedSprite2D.animation.play("collect")
+	collect.emit(value)
+	get_tree().create_timer(1.0)
+	queue_free()
+
+
+func _on_timeout() -> void:
+	$AnimatedSprite2D.animation.play(body)
