c60b188feb616aa566c500430f5102113fd37378
VeryNyu <gustavohuerta96@gmail.com>
2026-04-14 17:34:15 -0600

    Tentative Fruit Locations

--- Scripts/Data/config.gd
+++ Scripts/Data/config.gd
@@ -47,7 +47,26 @@ var DATA: Dictionary = {
 			"Banana": [],
 			"Melon": [],
 			"Pineapple": []
-		}
+		},
+		"Coin Locations": [
+			Vector2i(648, 136),
+			Vector2i(728, 136),
+			Vector2i(936, 136),
+			Vector2i(952, 136),
+			Vector2i(968, 136),
+			Vector2i(984, 136),
+			Vector2i(1096, 136),
+			Vector2i(664, 88),
+			Vector2i(680, 88),
+			Vector2i(696, 88),
+			Vector2i(712, 88),
+			Vector2i(1352, 88),
+			Vector2i(1368, 88),
+			Vector2i(1384, 88),
+			Vector2i(1400, 88),
+			Vector2i(1416, 88),
+			Vector2i(1432, 88),
+		]
 	},
 	2: {
 		"PlayerSpawnPoint": Vector2i(37, 101),
@@ -64,7 +83,15 @@ var DATA: Dictionary = {
 			"Banana": [],
 			"Melon": [],
 			"Pineapple": []
-		}
+		},
+		"Coin Locations": [
+			Vector2i(1704, 152),
+			Vector2i(1720, 88),
+			Vector2i(1736, 152),
+			Vector2i(1752, 88),
+			Vector2i(1768, 152),
+			Vector2i(1784, 88),
+		]
 	}
 }
 
